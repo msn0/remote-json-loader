925bfa4e58c1c5498f2f2b44b75cc65a0732ad14
Michał Jezierski <ambinanitelo@gmail.com>
2016-10-22 01:02:00 +0200

    loader

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const got = require('got');
 
+function requireFromString(src, filename) {
+  var Module = module.constructor;
+  var m = new Module();
+  m._compile(src, filename);
+  return m.exports;
+}
+
 module.exports = function(source) {
     this.cacheable();
     var callback = this.async();
     var query = this.query.slice(1);
     var vendors = JSON.parse(source);
-
+    var that = this;
     got(vendors[query])
         .then(response => {
-            callback(null, 'module.exports = ' + JSON.stringify(response.body) + ';');
+
+            console.log(requireFromString('module.exports = { test: 1}'));
+
+            // console.log(eval('module.exports = ' + response.body + ';'));
+            callback(null, 'module.exports = ' + response.body + ';');
         });
 };
