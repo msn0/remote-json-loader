fb1446524367b94fc331d8bc8471bb4a9526ab83
Michał Jezierski <ambinanitelo@gmail.com>
2016-10-22 22:25:20 +0200

    use const

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const got = require('got');
 
 function requireFromString(src, filename) {
-  var Module = module.constructor;
-  var m = new Module();
+  const Module = module.constructor;
+  const m = new Module();
   m._compile(src, filename);
   return m.exports;
 }
 
 module.exports = function (source) {
   this.cacheable();
-  var callback = this.async();
-  var query = this.query.slice(1);
-  var vendors = JSON.parse(source);
-  var that = this;
+
+  const callback = this.async();
+  const query = this.query.slice(1);
+  const vendors = JSON.parse(source);
+
   got(vendors[query])
     .then(response => {
-
       console.log(requireFromString('module.exports = { test: 1}'));
-
       // console.log(eval('module.exports = ' + response.body + ';'));
       callback(null, 'module.exports = ' + response.body + ';');
     });
