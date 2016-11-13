193cea71add5ef78131418e22549ba74184709e0
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-14 00:42:46 +0100

    remote json loader

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const got = require('got');
 
-function requireFromString(src, filename) {
-  const Module = module.constructor;
-  const m = new Module();
-  m._compile(src, filename);
-  return m.exports;
-}
-
 module.exports = function (source) {
   this.cacheable();
 
@@ -16,8 +9,6 @@ module.exports = function (source) {
 
   got(vendors[query])
     .then(response => {
-      console.log(requireFromString('module.exports = { test: 1}'));
-      // console.log(eval('module.exports = ' + response.body + ';'));
-      callback(null, 'module.exports = ' + response.body + ';');
+      callback(null, JSON.parse(response.body))
     });
 };
--- test.js
+++ test.js
@@ -1,28 +1,14 @@
 const test = require('ava');
 const loader = require('./');
 
-test.cb('should load json resource', t => {
+test.cb('should load remote json', t => {
   loader.call({
-    cacheable: () => {},
-    async: () => {
-      return function (a, Cow) {
-        t.is(Cow[0].Barcode, '8077676562');
-        t.end();
-      };
+    cacheable: () => {
+    },
+    async: () => (a, Cow) => {
+      t.is(Cow[0].Barcode, '8077676562');
+      t.end();
     },
     query: '?CowInCar'
   }, JSON.stringify({CowInCar: 'http://barcodefinder.com/search?q=8077676562&format=json'}));
-});
-
-test.cb('should load module resource', t => {
-  loader.call({
-    cacheable: () => {},
-    async: () => {
-      return function (a, ReactDOM) {
-        console.log(ReactDOM);
-        t.end();
-      };
-    },
-    query: '?react-dom-server'
-  }, JSON.stringify({'react-dom-server': 'https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom-server.js'}));
 });
