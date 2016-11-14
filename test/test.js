77dd5fb7b99e716f3d8375182194da3a7c0d4412
Michał Jezierski <ambinanitelo@gmail.com>
2016-11-14 19:39:08 +0100

    test/ for tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,15 @@
 const test = require('ava');
-const loader = require('./');
+const loader = require('../');
 
 test.cb('should load remote json', t => {
   loader.call({
-    cacheable: () => {
-    },
+    cacheable: () => {},
     async: () => (a, Cow) => {
       t.is(Cow[0].Barcode, '8077676562');
       t.end();
     },
     query: '?CowInCar'
-  }, JSON.stringify({CowInCar: 'http://barcodefinder.com/search?q=8077676562&format=json'}));
+  }, JSON.stringify({
+    CowInCar: 'http://barcodefinder.com/search?q=8077676562&format=json'
+  }));
 });
